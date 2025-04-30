56622727869f625119dee1fa0447b2e97b177085
DrMvF <mvf@ikigami.de>
2025-04-30 23:32:41 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,21 +14,21 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
+            Radical Sensitive Leadership{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
+              white.space
             </code>
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            Intuition. Intelligence. Integrity.
           </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="https://ikigami.beehiiv.com/"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -39,22 +39,22 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            Ikigami Insider
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="https://ikigami.beehiiv.com/subscribe"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Subscribe
           </a>
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://www.linkedin.com/in/miriamvonfelbert/"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -65,11 +65,11 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
+          LinkedIn
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://ikigami.beehiiv.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -80,11 +80,11 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Examples
+          Insider
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://ikigami.de/en/home-english/"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -95,7 +95,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Go to ikigami.de →
         </a>
       </footer>
     </div>
