145f4240e6b469189c607234226427e25d47ff9c
DrMvF <mvf@ikigami.de>
2025-06-13 08:41:50 +0000

    Update Landingpage Buttons

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col sm:flex-row">
         <a
           className="rounded-full border border-transparent transition-colors flex items-center justify-center bg-black text-white hover:bg-[#383838] dark:bg-white dark:text-black dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 min-w-[200px] text-center"
-          href="/blog"
+          href="https://ghost.radicalsensitiveleadership.com/"
         >
         Deep Flow
         </a>
