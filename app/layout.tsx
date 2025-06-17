2f08dc1fc2228a9545526fc247e820afb6c14122
DrMvF <mvf@ikigami.de>
2025-06-17 07:29:37 +0000

    Update layout with Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -38,6 +39,8 @@ export default function RootLayout({
         data-api="v3"
         crossOrigin="anonymous"
       />
+      {/* 👇 VERCEL ANALYTICS */}
+      <Analytics />
       </body>
     </html>
   );
